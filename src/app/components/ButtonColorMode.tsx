cd157f715545be07bd1f3f558001dfed27b1a816
Hromsi <bodak2011@yandex.ru>
2024-11-15 23:01:46 +0300

    feat: add icons for color mode button and change styles

--- src/app/components/ButtonColorMode.tsx
+++ src/app/components/ButtonColorMode.tsx
@@ -1,11 +1,14 @@
 'use client'
-import { Button, useColorMode } from "@chakra-ui/react";
+
+import { Button, Icon, useColorMode } from "@chakra-ui/react";
+import { SunIcon } from '@chakra-ui/icons'
+import { FaMoon } from "react-icons/fa";
 
 const ButtonColorMode = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   return (
-      <Button onClick={toggleColorMode}>
-        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      <Button onClick={toggleColorMode} variant="ghost" borderRadius="full">
+        <Icon as={colorMode === 'light' ? FaMoon : SunIcon} color="gray.500" />
       </Button>
   );
 }
